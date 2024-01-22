8acbaffa20eec263fc8080452201f6579d14ec73
zavaultraz <zavazaneta@gmail.com>
2024-01-22 14:18:12 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      //membuat custom spacing
+      spacing:{
+        13: '3.25rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
